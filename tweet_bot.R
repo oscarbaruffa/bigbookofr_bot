7f553c4759b1fd37a223a8e99abf5faf2609855f
oscarbaruffa <16580713+oscarbaruffa@users.noreply.github.com>
2022-01-02 11:13:38 +0100

    testing creds

--- tweet_bot.R
+++ tweet_bot.R
@@ -2,7 +2,10 @@
 # Load libraries ----------------------------------------------------------
 
 library("googlesheets4")
-
+library("rtweet")
+library("dplyr")
+library("tidyr")
+library("stringr")
 
 
 
@@ -12,7 +15,37 @@ library("googlesheets4")
 
 gs4_deauth()
 books_source <- read_sheet("https://docs.google.com/spreadsheets/d/1vufdtrIzF5wbkWZUG_HGIBAXpT1C4joPx2qTh5aYzDg",
-                           sheet = "books")
+                           sheet = "books") %>% 
+  separate_rows(chapters, sep = ";")
 
 
 
+
+# Choose book and generate url-------------------------------------------------------------
+
+
+book <- sample_n(books_source, 1) %>% 
+  mutate(url = paste0("https://bigbookofr.com/",
+                      str_replace_all(str_to_lower(chapters), " ", "-"),
+                      ".html#",
+                      str_replace_all(str_to_lower(title), " ", "-")))
+
+
+
+
+# Send tweet --------------------------------------------------------------
+
+
+# 
+# Create a token containing your Twitter keys
+rtweet::create_token(
+  app = "BigBookofR",  # the name of the Twitter app
+  consumer_key = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
+
+# Example: post a tweet via the API
+# The keys will are in your environment thanks to create_token()
+rtweet::post_tweet(status = "Hello, world5")
