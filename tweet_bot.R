aec9b9018555eedbf4c6905a8afb1d54b04ab780
oscarbaruffa <16580713+oscarbaruffa@users.noreply.github.com>
2022-01-07 08:38:38 +0100

    changes api key and trying to fix seperator

--- tweet_bot.R
+++ tweet_bot.R
@@ -19,7 +19,8 @@ library("stringr")
 gs4_deauth()
 books_source <- read_sheet("https://docs.google.com/spreadsheets/d/1vufdtrIzF5wbkWZUG_HGIBAXpT1C4joPx2qTh5aYzDg",
                            sheet = "books") %>% 
-  separate_rows(chapters, sep = ";")
+  separate_rows(chapters, sep = ";") %>% 
+  mutate(chapters <- str_trim(chapters, side=c("both")))
 
 
 
@@ -42,10 +43,10 @@ book_status = paste(book[1, "title"], book[1, "url"], sep="\n")
 # Create a token containing your Twitter keys
 rbot_token <- rtweet::create_token(
   app = "bigbookofr",  # the name of the Twitter app
-  consumer_key = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
-  consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
-  access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
-  access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+  consumer_key = Sys.getenv("RBOT_TWITTER_CONSUMER_API_KEY"),
+  consumer_secret = Sys.getenv("RBOT_TWITTER_CONSUMER_API_SECRET"),
+  access_token = Sys.getenv("RBOT_TWITTER_ACCESS_TOKEN"),
+  access_secret = Sys.getenv("RBOT_TWITTER_ACCESS_TOKEN_SECRET")
 )
 
 # Example: post a tweet via the API
