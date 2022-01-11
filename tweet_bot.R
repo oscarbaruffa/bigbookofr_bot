081a1475045169c1a227a81eaaa80c8b232f3052
Isabella Velasquez <ivelasq@gmail.com>
2022-01-10 16:24:30 -0800

    Address Issue #3

--- tweet_bot.R
+++ tweet_bot.R
@@ -1,7 +1,5 @@
 # Based on this blog post: https://www.rostrum.blog/2020/09/21/londonmapbot/
 
-
-
 # Load libraries ----------------------------------------------------------
 
 library("googlesheets4")
@@ -10,59 +8,66 @@ library("dplyr")
 library("tidyr")
 library("stringr")
 
-
 # Comment to trigger diff for testing.
 
 # Load data ---------------------------------------------------------------
 
-
 gs4_deauth()
-books_source <- read_sheet("https://docs.google.com/spreadsheets/d/1vufdtrIzF5wbkWZUG_HGIBAXpT1C4joPx2qTh5aYzDg",
-                           sheet = "books") %>% 
-  separate_rows(chapters, sep = ";") %>% 
+books_source <-
+  read_sheet(
+    "https://docs.google.com/spreadsheets/d/1vufdtrIzF5wbkWZUG_HGIBAXpT1C4joPx2qTh5aYzDg",
+    sheet = "books"
+  ) %>%
+  separate_rows(chapters, sep = ";") %>%
   mutate(chapters = str_trim(chapters, side = "both"))
-
 
 head(books_source)
 
 # Choose book and generate url-------------------------------------------------------------
 
-
-book <- sample_n(books_source, 1) %>% 
-  mutate(url = paste0("https://bigbookofr.com/",
-                      str_replace_all(str_to_lower(chapters), " ", "-"),
-                      ".html#",
-                      str_replace_all(str_to_lower(title), " ", "-")))
+book <- sample_n(books_source, 1) %>%
+  mutate(url = paste0(
+    "https://bigbookofr.com/",
+    str_replace_all(str_to_lower(chapters), " ", "-"),
+    ".html#",
+    str_replace_all(str_to_lower(title), " ", "-")
+  ))
 
 book
 
-book_status = paste(book[1, "title"], "#RStats", book[1, "url"], sep="\n")
+authors <-
+  book %>% 
+  select(contains("author")) %>% 
+  purrr::flatten_chr()
+
+#book_status = paste(book[1, "title"], "#RStats", book[1, "url"], sep="\n")
+
+book_status <-
+  book %>%
+  glue::glue_data("{title}",
+                  " by ",
+                  glue::glue_collapse(na.omit(authors), sep = ", ", last = " and "),
+                  "
+                  
+                  {url}
+                  #RStats")
 
 book_status
 
 # Send tweet --------------------------------------------------------------
 
-
-#Create a token containing your Twitter keys
+# Create a token containing your Twitter keys
 rbot_token <- rtweet::create_token(
-  app = "BigBookofR",  # the name of the Twitter app
+  app = "BigBookofR",
+  # the name of the Twitter app
   consumer_key = Sys.getenv("RBOT_TWITTER_CONSUMER_API_KEY"),
   consumer_secret = Sys.getenv("RBOT_TWITTER_CONSUMER_API_SECRET"),
   access_token = Sys.getenv("RBOT_TWITTER_ACCESS_TOKEN"),
   access_secret = Sys.getenv("RBOT_TWITTER_ACCESS_TOKEN_SECRET"),
-  set_renv = FALSE)
-
-# auth <-  rtweet_bot(
-#   api_key = Sys.getenv("RBOT_TWITTER_CONSUMER_API_KEY"),
-#   api_secret = Sys.getenv("RBOT_TWITTER_CONSUMER_API_SECRET"),
-#   access_token = Sys.getenv("RBOT_TWITTER_ACCESS_TOKEN"),
-#   access_secret = Sys.getenv("RBOT_TWITTER_ACCESS_TOKEN_SECRET")
-# )
-# 
-# auth_as(auth)
-
+  set_renv = FALSE
+)
 
 # Example: post a tweet via the API
-# The keys will are in your environment thanks to create_token()
+# The keys are in your environment thanks to create_token()
 rtweet::post_tweet(status = book_status,
                    token = rbot_token)
